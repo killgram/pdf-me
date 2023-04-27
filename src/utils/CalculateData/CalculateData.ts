9b7c9f2fd0a9768b00a9cb08f1656672aaa4b57e
killgram <killgram94@gmail.com>
2023-04-27 20:06:24 +0400

    add a bit logic into calculate data method

--- src/utils/CalculateData/CalculateData.ts
+++ src/utils/CalculateData/CalculateData.ts
@@ -1,6 +1,10 @@
 import { countForm } from "../CountForm";
 
-const calculateData = (commonData: any, data: any): Object => {
+const calculateData = (
+  commonData: any,
+  data: any,
+  personalizeData: any
+): Object => {
   const age = calcAge(
     commonData.age.year,
     commonData.age.month,
@@ -13,9 +17,36 @@ const calculateData = (commonData: any, data: any): Object => {
     data.personalPart.ageForm.t3,
   ]);
 
+  const rebasePersonalizeData: any = {};
+  personalizeData.forEach((v: any) => {
+    rebasePersonalizeData[Object.keys(v)[0]] = Object.values(v)[0];
+  });
+
+  const telegramTitle: string = `@${
+    rebasePersonalizeData.telegram.split("/").reverse()[0]
+  }`;
+
+  const linkedInTitle: string = rebasePersonalizeData.linkedIn
+    .split("/")
+    .reverse()
+    .filter((v: string) => v)[0];
+
+  const githubTitle: string = rebasePersonalizeData.authorGit
+    .split("/")
+    .reverse()[0];
+
   return {
     age: age,
     ageForm: ageForm,
+    phone: rebasePersonalizeData.phone,
+    email: rebasePersonalizeData.email,
+    telegramLink: rebasePersonalizeData.telegram,
+    telegramTitle: telegramTitle,
+    linkedInLink: rebasePersonalizeData.linkedIn,
+    linkedInTitle: linkedInTitle,
+    githubLink: rebasePersonalizeData.authorGit,
+    githubTitle: githubTitle,
+    cvSiteLink: rebasePersonalizeData.cvCite,
   };
 };
 
